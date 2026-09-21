21201115738c921fb778c50840c1e6ca21124e71
lyn-0 <108858801+lyn-0@users.noreply.github.com>
2026-09-21 16:16:22 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js - Service Worker（オフライン対応）
 // ネットワーク優先でファイルを取得し、オフライン時のみキャッシュを使います
 
-const CACHE_NAME = 'playbudget-v4'; // ★更新のたびに番号を上げる
+const CACHE_NAME = 'yenly-v5'; // ★更新のたびに番号を上げる
 
 // キャッシュするファイル一覧
 const FILES_TO_CACHE = [
